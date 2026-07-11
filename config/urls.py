583cbedcc1764d897bf914adc66659d6ecf5458e
Gaurav Pant <gauravpant870@gmail.com>
2026-07-11 14:58:18 +0545

    home path

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("chat/", include("chat.urls")),
     path("friends/", include("friends.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("", include("accounts.urls")),
 ]
