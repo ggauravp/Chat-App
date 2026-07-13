968c59751eba414500b9480b5734b231862e5197
Gaurav Pant <gauravpant870@gmail.com>
2026-07-13 16:04:49 +0545

    workflow added

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,6 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-# Application definition
-
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
